c42a3b2f14c89a153439759682168ab7bc435d25
alexgc <alex.gilsoncampillo@gmail.com>
2023-08-24 10:40:53 +0200

    Fixed getBooks

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 var db *sql.DB
 
-type book struct {
+type Book struct {
 	ID            int    `json:"id"`
 	Title         string `json:"title"`
 	Author        string `json:"author"`
@@ -45,51 +45,22 @@ func main() {
 	fmt.Println("Connected to mysql server.")
 
 	router := gin.Default()
-	router.GET("/books", getBooks2)
+	router.GET("/books", getBooks)
 	router.GET("/books/:id", getBookByID)
 	router.POST("/books", postBooks)
-	router.Run("localhost:8080")
+	router.Run(":8080")
 }
-
-// func getBooks(c *gin.Context) {
-
-// 	var books []book
-
-// 	rows, err := db.Query("SELECT * FROM book")
-// 	if err != nil {
-// 		c.IndentedJSON(http.StatusInternalServerError, "")
-// 		return
-// 	}
-// 	defer rows.Close()
-// 	// Loop through rows, using Scan to assign column data to struct fields.
-// 	for rows.Next() {
-// 		var alb book
-// 		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Author, &alb.PublishedDate); err != nil {
-// 			c.IndentedJSON(http.StatusInternalServerError, "")
-// 			return
-// 		}
-// 		books = append(books, alb)
-// 	}
-// 	if err := rows.Err(); err != nil {
-// 		c.IndentedJSON(http.StatusInternalServerError, "")
-// 		return
-// 	}
-// 	c.IndentedJSON(http.StatusOK, books)
-// }
 
 func postBooks(c *gin.Context) {
 
-	var newBook book
+	var newBook Book
 	if err := c.BindJSON(&newBook); err != nil {
 		return
 	}
-	_, err := db.Exec("INSERT INTO book (title, author, published_date) VALUES (?, ?, ?)", newBook.Title, newBook.Author, newBook.PublishedDate)
+	_, err := db.Exec("INSERT INTO book (title, author, published_date, genre) VALUES (?, ?, ?, ?)", newBook.Title, newBook.Author, newBook.PublishedDate, newBook.Genre)
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, "")
 		return
-	}
-	if err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, "")
 	}
 
 	c.IndentedJSON(http.StatusCreated, newBook)
@@ -99,95 +70,65 @@ func postBooks(c *gin.Context) {
 func getBookByID(c *gin.Context) {
 
 	id := c.Param("id")
-	var alb book
+	var book Book
 
 	row := db.QueryRow("SELECT * FROM book WHERE id = ?", id)
-	if err := row.Scan(&alb.ID, &alb.Title, &alb.Author, &alb.PublishedDate); err != nil {
+	if err := row.Scan(&book.ID, &book.Title, &book.Author, &book.PublishedDate, &book.Genre); err != nil {
 		if err == sql.ErrNoRows {
-			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
 			return
 		}
 		c.IndentedJSON(http.StatusInternalServerError, "")
 		return
 	}
-	c.IndentedJSON(http.StatusOK, alb)
+	c.IndentedJSON(http.StatusOK, book)
 }
 
-// booksByAuthor queries for books that have the specified author name.
-func getBooksByAuthor(c *gin.Context) {
-
-	name := c.Param("author")
-
-	var books []book
-
-	rows, err := db.Query("SELECT * FROM book WHERE author = ?", name)
-	if err != nil {
-		// return nil, fmt.Errorf("booksByAuthor %q: %v", name, err)
-		c.IndentedJSON(http.StatusInternalServerError, "")
-		return
-	}
-	defer rows.Close()
-	// Loop through rows, using Scan to assign column data to struct fields.
-	for rows.Next() {
-		var alb book
-		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Author, &alb.PublishedDate); err != nil {
-			// return nil, fmt.Errorf("booksByAuthor %q: %v", name, err)
-			c.IndentedJSON(http.StatusInternalServerError, "")
-			return
-		}
-		books = append(books, alb)
-	}
-	if err := rows.Err(); err != nil {
-		// return nil, fmt.Errorf("booksByAuthor %q: %v", name, err)
-		c.IndentedJSON(http.StatusInternalServerError, "")
-		return
-	}
-	// return books, nil
-	c.IndentedJSON(http.StatusOK, books)
-}
-
-func getBooks2(c *gin.Context) {
-	var books []book
+func getBooks(c *gin.Context) {
+	var books []Book
 
 	// Build the SQL query with optional filters
 	query := "SELECT * FROM book WHERE 1=1"
 	params := c.Request.URL.Query()
 
+	if id := params.Get("id"); id != "" {
+		query += " AND id = " + id
+	}
+
 	if title := params.Get("title"); title != "" {
-		query += " AND title = ?"
+		query += " AND title = \"" + title + "\""
 	}
 
 	if author := params.Get("author"); author != "" {
-		query += " AND author = ?"
+		query += " AND author = \"" + author + "\""
 	}
 
 	if genre := params.Get("genre"); genre != "" {
-		query += " AND genre = ?"
+		query += " AND genre = \"" + genre + "\""
 	}
 
 	if fromDate := params.Get("published_from"); fromDate != "" {
-		query += " AND published_date >= ?"
+		query += " AND published_date >= \"" + fromDate + "\""
 	}
 
 	if toDate := params.Get("published_to"); toDate != "" {
-		query += " AND published_date <= ?"
+		query += " AND published_date <= \"" + toDate + "\""
 	}
 
-	rows, err := db.Query(query, params.Get("title"), params.Get("author"), params.Get("genre"), params.Get("published_from"), params.Get("published_to"))
+	rows, err := db.Query(query)
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, "")
 		return
 	}
 	defer rows.Close()
 
-	// Loop through rows, using Scan to assign column data to struct fields.
 	for rows.Next() {
-		var alb book
-		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Author, &alb.PublishedDate, &alb.Genre); err != nil {
+		var book Book
+		if err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.PublishedDate, &book.Genre); err != nil {
 			c.IndentedJSON(http.StatusInternalServerError, "")
 			return
 		}
-		books = append(books, alb)
+		books = append(books, book)
 	}
 
 	if err := rows.Err(); err != nil {
@@ -195,5 +136,9 @@ func getBooks2(c *gin.Context) {
 		return
 	}
 
+	if len(books) == 0 {
+		c.IndentedJSON(http.StatusOK, []Book{})
+		return
+	}
 	c.IndentedJSON(http.StatusOK, books)
 }
